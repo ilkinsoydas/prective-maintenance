27a3e607296f96647954aa5c512f2ea256767179
“ilkinsoydas” <ilkinsoydas123@gmail.com>
2026-04-27 20:51:13 +0300

    Logistic regression yöntemini denedim. Bu yöntemle fit yaptığımda score çok yüksek çıkıyor fakat Accuracy Paradox doğuyor. Zaten verinin %99'u sağlam  olduğu için model tembellik yapıyor ve oran 0.99 çıkıyor. Confusion matrix kullanarak test ettiğimde arızaları yakalayamadığını gözlemledim. Logistic regression'ın bu dengesiz veri projesine uygun olmadığına karar verdim. Bir sonraki adımda ağaç tabanlı farklı bir algoritma deneyeceğim.

---
+++ predictiveMaintenance.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+
+pd.set_option("display.max_columns", None)
+
+df = pd.read_csv("predictive_maintenance.csv")
+print(df.info())
+
+le = LabelEncoder()
+df["device"] = le.fit_transform(df["device"])
+
+df["date"] = pd.to_datetime(df["date"])
+df["day"] = df["date"].dt.day
+df["month"] = df["date"].dt.month
+df["day_of_week"] = df["date"].dt.dayofweek
+df = df.drop(columns=["date"])
+
+print(df.head())
+print(df["failure"].value_counts())
+
+y = df["failure"]
+x = df.drop(columns=["failure"])
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.88, random_state=42, stratify=y)
+
+model = LogisticRegression()
+model.fit(x_train, y_train)
+print(model.score(x_test, y_test)) #Accuracy Paradox, zaten verilerin %90'ı 0 sınıfında. O yüzden model tembellik yaptı ve oran 0.99 çıktı
+
+y_pred = model.predict(x_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+
+
+
+
+
+
+
+
+
+
