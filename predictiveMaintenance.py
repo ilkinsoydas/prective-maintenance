fd9659ad9f72f02912d9c1e6bc52b5f318bbb011
“ilkinsoydas” <ilkinsoydas123@gmail.com>
2026-04-28 21:10:42 +0300

    XGBoost modeli ekledim ve scale_pos_weight uyguladım ancak recall 0 geldi. Arızalı veri çok az olduğundan bu durum gerçekleşti

--- predictiveMaintenance.py
+++ predictiveMaintenance.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+#from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, classification_report
+from xgboost import XGBClassifier
 
 pd.set_option("display.max_columns", None)
 
@@ -27,13 +28,28 @@ x = df.drop(columns=["failure"])
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.88, random_state=42, stratify=y)
 
-model = LogisticRegression()
+#model = LogisticRegression()
+#model.fit(x_train, y_train)
+#print(model.score(x_test, y_test)) #Accuracy Paradox, zaten verilerin %90'ı 0 sınıfında. O yüzden model tembellik yaptı ve oran 0.99 çıktı
+
+#y_pred = model.predict(x_test)
+#cm = confusion_matrix(y_test, y_pred)
+#print(cm)
+
+rate = len(y_train[y_train==0]) / len(y_train[y_train==1]) #sağlam/arızalı
+print(rate)
+
+model = XGBClassifier(scale_pos_weight = rate, random_state = 42)
 model.fit(x_train, y_train)
-print(model.score(x_test, y_test)) #Accuracy Paradox, zaten verilerin %90'ı 0 sınıfında. O yüzden model tembellik yaptı ve oran 0.99 çıktı
+print(model.score(x_test, y_test))
 
 y_pred = model.predict(x_test)
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
+
+print("\nKarmaşıklık Matrisi\n")
+print(confusion_matrix(y_test, y_pred))
+print("\nSınıflandırma Raporu\n")
+print(classification_report(y_test, y_pred))
+
 
 
 
