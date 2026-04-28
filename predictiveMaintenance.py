0d6142696ec614d9b5b075226b7c69b30aa6d45e
“ilkinsoydas” <ilkinsoydas123@gmail.com>
2026-04-28 22:05:39 +0300

    SMOTE entegre edildi, arıcalar yakalanmaya başlandı, recall değerimiz arttı

--- predictiveMaintenance.py
+++ predictiveMaintenance.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 #from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report
 from xgboost import XGBClassifier
+from imblearn.over_sampling import SMOTE
 
 pd.set_option("display.max_columns", None)
 
@@ -36,12 +37,34 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.88, rando
 #cm = confusion_matrix(y_test, y_pred)
 #print(cm)
 
-rate = len(y_train[y_train==0]) / len(y_train[y_train==1]) #sağlam/arızalı
-print(rate)
+#rate = len(y_train[y_train==0]) / len(y_train[y_train==1]) #sağlam/arızalı
+#print(rate)
 
-model = XGBClassifier(scale_pos_weight = rate, random_state = 42)
-model.fit(x_train, y_train)
-print(model.score(x_test, y_test))
+#model = XGBClassifier(scale_pos_weight = rate, random_state = 42)
+#model.fit(x_train, y_train)
+#print(model.score(x_test, y_test))
+
+#y_pred = model.predict(x_test)
+
+#print("\nKarmaşıklık Matrisi\n")
+#print(confusion_matrix(y_test, y_pred))
+#print("\nSınıflandırma Raporu\n")
+#print(classification_report(y_test, y_pred))
+
+smote = SMOTE(random_state=42)
+
+print("\nSMOTE ÖNCESİ\n")
+print("Arıza: ", sum(y_train == 1))
+print("\nSağlam: ", sum(y_train == 0))
+
+x_train_smote, y_train_smote = smote.fit_resample(x_train, y_train)
+
+print("\nSMOTE SONRASI\n")
+print("Arıza: ", sum(y_train_smote == 1))
+print("\nSağlam: ", sum(y_train_smote == 0))
+
+model = XGBClassifier(random_state=42)
+model.fit(x_train_smote, y_train_smote)
 
 y_pred = model.predict(x_test)
 
@@ -49,6 +72,8 @@ print("\nKarmaşıklık Matrisi\n")
 print(confusion_matrix(y_test, y_pred))
 print("\nSınıflandırma Raporu\n")
 print(classification_report(y_test, y_pred))
+
+
 
 
 
