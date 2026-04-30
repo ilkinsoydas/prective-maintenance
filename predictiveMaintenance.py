50f9170152894283267d353156c6eb9ab2723239
“ilkinsoydas” <ilkinsoydas123@gmail.com>
2026-04-30 23:52:31 +0300

    feat: son optimizasyon.

--- predictiveMaintenance.py
+++ predictiveMaintenance.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-#from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report
 from xgboost import XGBClassifier
 from imblearn.over_sampling import SMOTE
@@ -33,27 +32,6 @@ x = df.drop(columns=["failure"])
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.88, random_state=42, stratify=y)
 
-#model = LogisticRegression()
-#model.fit(x_train, y_train)
-#print(model.score(x_test, y_test)) #Accuracy Paradox, zaten verilerin %90'ı 0 sınıfında. O yüzden model tembellik yaptı ve oran 0.99 çıktı
-
-#y_pred = model.predict(x_test)
-#cm = confusion_matrix(y_test, y_pred)
-#print(cm)
-
-#rate = len(y_train[y_train==0]) / len(y_train[y_train==1]) #sağlam/arızalı
-#print(rate)
-
-#model = XGBClassifier(scale_pos_weight = rate, random_state = 42)
-#model.fit(x_train, y_train)
-#print(model.score(x_test, y_test))
-
-#y_pred = model.predict(x_test)
-
-#print("\nKarmaşıklık Matrisi\n")
-#print(confusion_matrix(y_test, y_pred))
-#print("\nSınıflandırma Raporu\n")
-#print(classification_report(y_test, y_pred))
 
 smote = SMOTE(random_state=42)
 
@@ -67,15 +45,6 @@ print("\nSMOTE SONRASI\n")
 print("Arıza: ", sum(y_train_smote == 1))
 print("\nSağlam: ", sum(y_train_smote == 0))
 
-"""model = XGBClassifier(random_state=42)
-model.fit(x_train_smote, y_train_smote)
-
-y_pred = model.predict(x_test)
-
-print("\nKarmaşıklık Matrisi\n")
-print(confusion_matrix(y_test, y_pred))
-print("\nSınıflandırma Raporu\n")
-print(classification_report(y_test, y_pred))"""
 
 parameter_grid = {
     "max_depth": [2, 3, 4],
